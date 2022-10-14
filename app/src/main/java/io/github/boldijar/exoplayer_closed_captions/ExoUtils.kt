711e61fcba1535951fa73d4c3e1e659128cc9e47
Paul Boldijar <boldijar.paul@gmail.com>
2022-10-14 21:29:00 +0800

    Filtering for text tracks.

--- app/src/main/java/io/github/boldijar/exoplayer_closed_captions/ExoUtils.kt
+++ app/src/main/java/io/github/boldijar/exoplayer_closed_captions/ExoUtils.kt
@@ -1,20 +1,27 @@
 package io.github.boldijar.exoplayer_closed_captions
 
+import com.google.android.exoplayer2.C.TRACK_TYPE_TEXT
 import com.google.android.exoplayer2.Tracks
 import java.util.*
 
 object ExoUtils {
     fun getLanguagesFromTrackInfo(track: Tracks): List<Language> {
-        return track.groups.asSequence().map {
+        return track.groups.asSequence().mapNotNull {
+            if (it.type != TRACK_TYPE_TEXT) {
+                return@mapNotNull null
+            }
             val group = it
             val formats = (0 until group.length).map { index -> group.getTrackFormat(index) }
             formats.mapNotNull { format ->
                 format.language
             }
-        }.flatten().toSet().toList().map {
-            val locale = Locale(it)
-            val languageName = locale.getDisplayLanguage(locale)
-            Language(code = it, name = languageName)
-        }.toList()
+        }
+            .flatten()
+            .toSet()
+            .map {
+                val locale = Locale(it)
+                val languageName = locale.getDisplayLanguage(locale)
+                Language(code = it, name = languageName)
+            }.toList()
     }
 }
